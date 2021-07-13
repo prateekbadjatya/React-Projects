24ac30a27066c7f521e8920a19c1be1557b7f48e
prateekbadjatya <41595806+prateekbadjatya@users.noreply.github.com>
2021-07-14 00:51:25 +0530

    add book component

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import './index.css'
 // import App from "./App";
 
 // stateless functional compoenent always return jsx
@@ -9,7 +11,7 @@ import ReactDOM from 'react-dom'
 
 // always return JSX
 
-const Greeting = () => <h1>Hello This is my first container</h1>
+// const Greeting = () => <h1>Hello This is my first container</h1>
 
 // https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=Q&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Creact&prettier=false&targets=&version=7.14.7&externalPlugins=
 
@@ -46,4 +48,81 @@ JSX RULE
 formatting
 */
 
+/* 
+Nested component 
+*/
+/* 
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  )
+}
+ */
+/* const Person = () => <h2>Prateek</h2>
+
+const Message = () => <p>Hello Prateek</p> */
+
+/* 
+
 ReactDOM.render(<Greeting />, document.getElementById('root'))
+ */
+
+const src =
+  'https://images-eu.ssl-images-amazon.com/images/I/81BdZ3PbmjL._AC_UL200_SR200,200_.jpg'
+
+const author = 'Prateek'
+
+const title = 'My Big Book of Creative Crayons'
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book seller='Amazon' price='32$' />
+      <Book seller='Amazon' price='32$' />
+      {/* <Book />
+      <Book />
+      <Book />
+      <Book /> */}
+    </section>
+  )
+}
+
+const Book = props => {
+  console.log('props value', props)
+  return (
+    <article className='book'>
+      {/* <Image /> <Title /> <Author /> */}
+      <img src={src} alt='' />
+
+      <h1>{title}</h1>
+      <h4
+        style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}
+      >
+        {author.toUpperCase()}
+      </h4>
+      <p
+        style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}
+      >
+        {props.price}
+      </p>
+    </article>
+  )
+}
+
+/* const Image = () => {
+  let src =
+    'https://images-eu.ssl-images-amazon.com/images/I/81BdZ3PbmjL._AC_UL200_SR200,200_.jpg'
+  return <img src={src} alt='' />
+} */
+
+/* const Title = () => <h1>My Big Book of Creative Crayons</h1> */
+
+/* const Author = () => (
+  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
+    {'Prateek'.toUpperCase()}
+  </h4>
+) */
+
+ReactDOM.render(<BookList />, document.getElementById('root'))
